427cfc221c6fb8895e0e598fc05c7242490003be
Christian Fischer <christian@chfi.se>
2016-03-29 15:31:17 +0200

    Document Aes module

--- lib/aes.mli
+++ lib/aes.mli
@@ -1,11 +1,15 @@
 val decrypt_ecb : int array -> int array -> int array
-(** ciphertext key **)
+(** [decrypt_ecb ciphertext key] decrypts [ciphertext] using AES-128
+    with chaining mode ECB and key [key], pads using PKCS#7 **)
 
 val encrypt_ecb : int array -> int array -> int array
-(** plaintext key **)
+(** [encrypt_ecb plaintext key] encrypts [plaintext] using AES-128
+    with chaining mode ECB and key [key], pads using PKCS#7 **)
 
 val decrypt_cbc : int array -> int array -> int array -> int array
-(** ciphertext key iv **)
+(** [decrypt_cbc ciphertext key iv] decrypts [ciphertext] using AES-128
+    with chaining mode CBC, IV [iv], and key [key], pads using PKCS#7 **)
 
 val encrypt_cbc : int array -> int array -> int array -> int array
-(** plaintext key iv **)
+(** [encrypt_cbc plaintext key iv] encrypts [plaintext] using AES-128
+    with chaining mode CBC, IV [iv], and key [key], pads using PKCS#7 **)
