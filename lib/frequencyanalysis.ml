cf32b979d64aee764687b444ec42f3b40a98c6f5
Christian Fischer <christian@chfi.se>
2016-03-29 13:34:06 +0200

    Minor cleanup

--- lib/frequencyanalysis.ml
+++ lib/frequencyanalysis.ml
@@ -38,9 +38,9 @@ let getcharfreq c =
 
 let count_non_alphanumerics str =
   let chars =
-    List.init 10 ~f:(fun i -> char_of_int (48 + i)) @
-    List.init 26 ~f:(fun i -> char_of_int (65 + i)) @
-    List.init 26 ~f:(fun i -> char_of_int (97 + i)) @
+    List.init ~f:(fun i -> char_of_int (48 + i)) 10 @
+    List.init ~f:(fun i -> char_of_int (65 + i)) 26 @
+    List.init ~f:(fun i -> char_of_int (97 + i)) 26 @
     ['.';'\'';',';'\"';' ';]
   in
   String.count ~f:(fun c -> not (List.mem chars c)) str
@@ -59,49 +59,50 @@ let score_string_chi2 str =
   let alphabet = "abcdefghijklmnopqrstuvwxyz" in
   (* first, calculate the count of each letter in the string. *)
   let count_alist =
-    List.map (String.to_list alphabet)
-             (fun c -> c, float_of_int (count_letter norm c))
+    List.map
+      ~f:(fun c -> (c, float_of_int (count_letter norm c)) )
+      (String.to_list alphabet)
   in
   let len = String.length norm in
 
   (* then, calculate the expected count of each letter in a string of that length. *)
   let expected_alist =
-    List.map (String.to_list alphabet)
-             (fun c -> c, getcharfreq c *. (float_of_int len))
+    List.map
+      ~f:(fun c -> (c, getcharfreq c *. (float_of_int len)) )
+      (String.to_list alphabet)
   in
 
   (* then take the sum of the square of the differences over the expected count,
      for each letter in the alphabet. *)
   let helper i =
-    let c = String.get alphabet i in
+    let c = alphabet.[i] in
     let expected = List.Assoc.find_exn expected_alist c in
     let count = List.Assoc.find_exn count_alist c in
     ((count -. expected) ** 2.0) /. expected
   in
 
-  let sqdifs = Array.init 26 helper in
+  let sqdifs = Array.init ~f:helper 26 in
   let nonalphacount = count_non_alphanumerics norm in
-  let calculated = Array.fold sqdifs ~init:0.0 ~f:(fun x y -> x +. y) in
+  let calculated = Array.fold ~init:0.0 ~f:(fun x y -> x +. y) sqdifs in
 
   calculated +. (float_of_int (nonalphacount * 100))
 
 let score_string_char str =
-  let normalized = String.lowercase str in
-  String.fold ~init:0.0 ~f:(fun acc c -> acc +. (getcharfreq c)) normalized
+  String.lowercase str
+  |> String.fold ~init:0.0 ~f:(fun acc c -> acc +. (getcharfreq c))
 
-(* bitwise hamming distance between int arrays,
-   exception thrown if arrays are of different lengths *)
+
 let hamming_distance ar1 ar2 =
   if (Array.length ar1) <> (Array.length ar2)
     then raise (Invalid_argument "Arrays must be of equal length");
+    (* helper to calculate the hamming distance between two bytes *)
   let single_dist i1 i2 =
-    (* looks at the 8 rightmost bits, since
-       we're treating the ints as eytes *)
     let diff = i1 lxor i2 in
-    let list = [0;1;2;3;4;5;6;7] in
-    List.count list ~f:(fun i -> if 1 land (diff lsr i) = 1 then true else false)
+    List.range 0 8 (* each bit must be checked *)
+    |> List.count ~f:(fun i -> 1 land (diff lsr i) = 1)
   in
-  Array.fold (Array.map2_exn ar1 ar2 single_dist) ~init:0 ~f:(+)
+  Array.fold ~init:0 ~f:(+) (Array.map2_exn ar1 ar2 single_dist)
+
 
 (* hamming distance normalized by division with array length *)
 let normalized_hamming_distance ar1 ar2 =
@@ -111,18 +112,20 @@ let normalized_hamming_distance ar1 ar2 =
   (float_of_int dist) /. (float_of_int (Array.length ar1))
 
 
+(* normalized hamming distance calculated for each neighboring pair of blocks *)
 let running_hamming_distance ar len =
-  (* remove the last few parts of the arrays, so that they can be evenly
-     split into 'len' sized blocks *)
   let blocks = Bytearray.split_every_n ar len in
   let indices = List.range 1 (List.length blocks) in
-  let sum = List.fold indices ~init:0.0
+  (* TODO: this function could really use some cleaning up, maybe with option *)
+  let sum = List.fold ~init:0.0
       ~f:(fun acc i -> acc +. (normalized_hamming_distance
                                  (List.nth_exn blocks (i-1))
                                  (List.nth_exn blocks i)))
+      indices
   in
   let num_blocks = List.length indices in
   sum /. (float_of_int num_blocks)
+
 
 let find_key_size ints maxsize =
   if maxsize > (Array.length ints) / 4 then raise (Failure "Too large keysize");
