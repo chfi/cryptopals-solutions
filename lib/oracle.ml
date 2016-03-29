88363ea0c7830c656b2e972638ca0f9a5aa8cd75
Christian Fischer <christian@chfi.se>
2016-03-29 15:33:14 +0200

    Cleanup Oracle module

--- lib/oracle.ml
+++ lib/oracle.ml
@@ -1,12 +1,11 @@
 open Core.Std
 
-let random_bytes len =
-  Array.init len ~f:(fun _ -> Random.int 256)
+let random_bytes =
+  Array.init ~f:(fun _ -> Random.int 256)
 
 (* TODO: refactor this to be specifically
    an ecb/cbc oracle, in name *)
 let ecb_or_cbc_oracle ?(key=(None)) plaintext =
-  (* Random.int 256 *)
   let module C = Cryptokit.Cipher in
   let key =
     match key with
@@ -14,7 +13,6 @@ let ecb_or_cbc_oracle ?(key=(None)) plaintext =
     | None   -> random_bytes 16
   in
 
-  (* let key = random_bytes 16 in *)
   let pre_len = (Random.int 6) + 5 in
   let suf_len = (Random.int 6) + 5 in
 
@@ -27,15 +25,13 @@ let ecb_or_cbc_oracle ?(key=(None)) plaintext =
   in
 
   let cbc = Random.bool () in
+
   if cbc
   then
     let iv = Array.init 16 ~f:(fun i -> 0) in
-    (* let iv = random_bytes 16 in *)
     Aes.encrypt_cbc padded key iv
   else
     Aes.encrypt_ecb padded key
 
 let ecb_encrypted ciphertext =
-  if (Frequencyanalysis.count_block_repeats ciphertext 16) > 1
-  then true
-  else false
+  (Frequencyanalysis.count_block_repeats ciphertext 16) > 1
